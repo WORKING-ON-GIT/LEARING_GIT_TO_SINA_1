1441c8e1c80dcadaed09c7b63794ca5933ef6ded
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-01-16 14:34:35 +0330

    Add Person Class!

---
+++ LEARING_GIT_TO_SINA/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LEARING_GIT_TO_SINA
+{
+	class Person
+	{
+	}
+}
